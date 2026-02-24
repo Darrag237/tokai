df0e68ece3d6903a287e0d093db60532e63b42f2
Nora Mohamed <126023637+noramohamed30@users.noreply.github.com>
2026-02-25 00:55:17 +0200

    Delete lib directory

--- lib/core/errors/app_exception.dart
+++
@@ -1,8 +0,0 @@
-class AppException implements Exception {
-  final String message;
-  final Object? cause;
-  AppException(this.message, {this.cause});
-
-  @override
-  String toString() => 'AppException(message: $message, cause: $cause)';
-}
